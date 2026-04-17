2431ad1ca2048e570c3c8c37aceee3c625707c70
alastair-simon <alastairfraser.work@gmail.com>
2026-04-17 16:00:48 +0200

    refactor: align constructor lines with text

--- src/Administration/Resources/app/administration/src/app/component/structure/sw-admin-menu-item/index.js
+++ src/Administration/Resources/app/administration/src/app/component/structure/sw-admin-menu-item/index.js
@@ -151,7 +151,10 @@ export default {
             }
 
             if (meta.$current) {
-                const [currentPath, ...ancestorPaths] = findAncestorPaths(meta.$current.path);
+                const [
+                    currentPath,
+                    ...ancestorPaths
+                ] = findAncestorPaths(meta.$current.path);
 
                 // Show "child active" only when a descendant is active, not the item itself.
                 return currentPath !== path && ancestorPaths.includes(path);
